91e495f9079caf90b16835f2f1e4b893dab8adf7
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-03-02 16:08:04 -0300

    criação do projeto

---
+++ src/main/java/com/devsuperior/des1/Des1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.des1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Des1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Des1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/des1/Des1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.des1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Des1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
